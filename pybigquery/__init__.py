696cd05b85a8a38618045fd715fc37cb441fd498
Jacob Hayes <jacob.r.hayes@gmail.com>
2020-09-26 03:01:44 -0400

    Support struct literals

--- pybigquery/__init__.py
+++ pybigquery/__init__.py
@@ -17,3 +17,44 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from sqlalchemy.dialects.postgresql import array
+from sqlalchemy.sql import expression, operators, sqltypes
+
+__all__ = ["array", "struct"]
+
+
+class STRUCT(sqltypes.Indexable, sqltypes.TypeEngine):
+    # NOTE: STRUCT names/types aren't currently supported.
+
+    __visit_name__ = "STRUCT"
+
+    class Comparator(sqltypes.Indexable.Comparator):
+        def _setup_getitem(self, index):
+            return operators.getitem, index, self.type
+
+    comparator_factory = Comparator
+
+
+class struct(expression.ClauseList, expression.ColumnElement):
+    """ Create a BigQuery struct literal from a collection of named expressions/clauses.
+    """
+    # NOTE: Struct subfields aren't currently propagated/validated.
+
+    __visit_name__ = "struct"
+
+    def __init__(self, clauses, field=None, **kw):
+        self.field = field
+        self.type = STRUCT()
+        super().__init__(*clauses, **kw)
+
+    def _bind_param(self, operator, obj, _assume_scalar=False, type_=None):
+        if operator is operators.getitem:
+            # TODO:
+            # - Validate field in clauses (or error if no clauses)
+            # - If the field is a sub-struct, return with all clauses, otherwise none.
+            return struct([], field=obj)
+
+    def self_group(self, against=None):
+        if not self.field and against in (operators.getitem,):
+            return expression.Grouping(self)
+        else:
+            return self

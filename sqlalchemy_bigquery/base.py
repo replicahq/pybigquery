0cf5ec1ef96f2c67e314a6eaab6de0507803e472
Jake Lazarus <jake@replicahq.com>
2022-02-08 12:46:39 -0600

    newline

--- sqlalchemy_bigquery/base.py
+++ sqlalchemy_bigquery/base.py
@@ -629,13 +629,9 @@ class BigQueryDDLCompiler(DDLCompiler):
         bq_opts = table.dialect_options["bigquery"]
         opts = []
         indices = ""
-        print("HEY!!!!")
-        print("bq_opts", bq_opts)
-        print(table)
-        breakpoint()
 
         if "clustering_fields" in bq_opts:
-            indices = f'CLUSTER BY {",".join(bq_opts.get("clustering_fields"))}'
+            indices = f'\nCLUSTER BY {",".join(bq_opts.get("clustering_fields"))}'
 
         if ("description" in bq_opts) or table.comment:
             description = process_string_literal(
